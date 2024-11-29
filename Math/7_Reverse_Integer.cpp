083db2e517302ea5b5aa93b51077742a1c7ce642
shamkharyan <156576229+shamkharyan@users.noreply.github.com>
2024-11-29 21:34:48 +0400

    feat: solve 0007 in c++

--- Math/7_Reverse_Integer.cpp
+++ Math/7_Reverse_Integer.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long y = 0;
+        int y = 0;
         while (x != 0)
         {
-            y = y * 10 + x % 10;
-            x /= 10;
+            if (y <= 214748364 && y >= -214748364)
+            {
+                y = y * 10 + x % 10;
+                x /= 10;
+            }
+            else if (y == 214748364 && x <= 7)
+                return y * 10 + x;
+            else
+                return 0;
         }
-        if (y > INT_MAX || y < INT_MIN)
-            return 0;
         return y;
     }
 };
