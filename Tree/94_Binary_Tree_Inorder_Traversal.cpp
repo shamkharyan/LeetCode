ccc0c5e92fb63f561a349fe512bd8ae8e888e3fd
Pavel Shamkharyan <156576229+shamkharyan@users.noreply.github.com>
2025-04-19 10:45:11 +0400

    feat: solve 0094 in c++

---
+++ Tree/94_Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */    
+class Solution {
+public:
+    void traverse(TreeNode* root, vector<int>& path)
+    {
+        if (root)
+        {
+            traverse(root->left, path);
+            path.push_back(root->val);
+            traverse(root->right, path);
+        }
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> path;
+        traverse(root, path);
+        return path;
+    }
+};
