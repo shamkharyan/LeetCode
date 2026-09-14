28d99ba7d8848a0a6af7c0e04e7742d075928894
Pavel Shamkharyan <156576229+shamkharyan@users.noreply.github.com>
2026-09-15 00:04:49 +0400

    feat: solve 0007 in c++

---
+++ medium/0007_reverse_integer/solution.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int reverse(int x)
+    {
+        int rev = 0;
+        while (x != 0)
+        {
+            int last = x % 10;
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && last > 7))
+                return 0;
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && last < -8))
+                return 0;
+            rev = 10 * rev + last;
+            x /= 10;
+        }
+        return rev;
+    }
+};
