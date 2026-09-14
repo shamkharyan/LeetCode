540cdc0f17010b338af680e4c46beeedd62ac00c
Pavel Shamkharyan <156576229+shamkharyan@users.noreply.github.com>
2026-09-14 22:16:28 +0400

    feat: solve 0111 in c++

---
+++ easy/0111_minimum_depth_of_binary_tree/solution.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int solve(TreeNode* root, int depth)
+    {
+        if (!root)
+            return depth;
+        if (!root->left && root->right)
+            return solve(root->right, depth + 1);
+        if (root->left && !root->right)
+            return solve(root->left, depth + 1);
+        return min(solve(root->left, depth + 1), solve(root->right, depth + 1));
+    }
+    
+    int minDepth(TreeNode* root)
+    {
+        return solve(root, 0);
+    }
+};
